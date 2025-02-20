e09f2eaffb14fb74dce967c4a8ed33312127f279
MicheleLafronza <michele.lafronza91@gmail.com>
2025-02-20 12:08:24 +0100

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
+            {{ __('Benvenuto') }} {{ Auth::user()->name }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg text-center">
+                <div class="p-6 text-gray-900">
+                    {{ __('Attualmente hai') }} {{ __('x') }} {{ __('progetti registrati.') }}
+                </div>
+
+                <div class="p-6 text-gray-900">
+                    {{ __('Hai ricevuto') }} {{ __('x') }} {{ __('mail.') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
